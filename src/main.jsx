bf864196abf2a8a4740f2e9d96472218335db6dd
Dmitriy <dmitriy@MacBook-Pro-14--Dmitriy.local>
2025-03-30 12:58:57 +0300

    🔥 Добавил wildcard маршрут для SPA

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import Wishlist from './components/Wishlist';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/wishlist/:id" element={<Wishlist />} />
-    </Routes>
+  <Route path="/wishlist/:id" element={<Wishlist />} />
+  <Route path="*" element={<Wishlist />} />   // <- эта строка критична!
+  </Routes>
   </BrowserRouter>
 )
