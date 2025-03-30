3b70218abab54f610289bf5e41b377d88c1677d3
Dmitriy <dmitriy@MacBook-Pro-14--Dmitriy.local>
2025-03-30 12:37:39 +0300

    Добавил 404.html для SPA fallback

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+  // 👇 Эта часть важна!
+  preview: {
+    port: 4173,
+    open: true,
+  }
+});
